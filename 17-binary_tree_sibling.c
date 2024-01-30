b1fdeffea5b074c8aef66ccdc3c0c7c88bd3c88e
Michael Iyke <ymichaelc@gmail.com>
2024-01-30 21:56:08 +0100

    Implemented the function binary_tree_sibling which inds the sibling of a node in a binary tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,112 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * power - the pow implementation
- * @base: base
- * @exponent: the exponent
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to a node
  *
- * Return: the power
+ * Return: pointer to the sibling of the node
  */
-double power(double base, int exponent)
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	double result = 1.0;
+	binary_tree_t *left, *right;
 
-	if (exponent < 0)
+	if (node && node->parent)
 	{
-		base = 1 / base;
-		exponent = -exponent;
+		left = node->parent->left;
+		right = node->parent->right;
+
+		if (right && right->n == node->n) /* node is right */
+			return (left);
+
+		if (left && left->n == node->n)
+			return (right);
 	}
-
-	while (exponent > 0)
-	{
-		if (exponent % 2 == 1)
-			result *= base;
-		base *= base;
-		exponent /= 2;
-	}
-
-	return (result);
-}
-
-/**
- * tree_height - measures the height of a binary tree
- * @tree: pointer to root node
- * Return: height of tree
- */
-size_t tree_height(const binary_tree_t *tree)
-{
-	size_t height_l;
-	size_t height_r;
-
-	if (tree == NULL)
-		return (0);
-
-	height_l = tree->left ? 1 + tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
-}
-
-/**
- * tree_size - measures the size of a binary tree
- * @tree: pointer to a binary tree
- *
- * Return: the size of the tree
- */
-size_t tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (1 + tree_size(tree->left) +
-		tree_size(tree->right));
-}
-
-/**
- * is_full -  checks if a binary tree is full
- * @tree: pointer to a binary tree
- *
- * Return: 0 for false and 1 for true
- */
-static int is_full(const binary_tree_t *tree)
-{
-	/**
-	 * A binary tree is full if each node has extacly 0 or two children.
-	 * There is no node with only left or right child.
-	 * They either have both or none at all
-	 */
-	if (tree == NULL)
-		return (1);
-
-	if (tree->left && tree->right == NULL)
-		return (0);
-	else if (tree->right && tree->left == NULL)
-		return (0);
-
-	return (is_full(tree->left) && is_full(tree->right));
-}
-
-/**
- * binary_tree_is_perfect -   checks if a binary tree is perfect
- * @tree: pointer to a binary tree
- *
- * Return: 0 for false and 1 for true
- *
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	/**
-	 * A binary tree is perfect if all nodes internal nodes (non-leafs)
-	 * are completely filled unless of course they're external nodes
-	 * (leafs to be precise).
-	 * Also, number of nodes = 2^(height + 1)-1
-	 * If this last property isn't satisfied, it's not a perfect tree
-	 */
-	size_t t_size, t_height;
-
-	t_size = tree_size(tree);
-	t_height = tree_height(tree);
-	if (t_size != power(2, (t_height + 1)) - 1)
-		return (0);
-	if (!is_full(tree))
-		return (0);
-	return (1);
+	return (NULL);
 }
