73cc7894ce42a3b29120eb0a842e768f899c9137
duncmv <dtindiwensi@gmail.com>
2024-02-01 00:00:56 +0300

    task 24

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,16 +19,3 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	return (node);
 }
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to a binary tree
- *
- * Return: the size of the tree
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (1 + binary_tree_size(tree->left) +
-		binary_tree_size(tree->right));
-}
--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to a binary tree
+ *
+ * Return: the size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) +
+		binary_tree_size(tree->right));
+}
+/**
  * tree_preorder - goes through a binary tree using pre-order traversal
  * @array: array to store node values
  * @index: index of array
