b65f11ea084954574bec88cf268de1975566c304
duncmv <dtindiwensi@gmail.com>
2024-02-01 14:29:26 +0300

    test

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,130 +1,47 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to a binary tree
- *
- * Return: the size of the tree
+ * bst_lesser - BST validator helper function lesser
+ * @node: node to compare
+ * @val: value to compare with
+ * Return: 1 if valid BST, else 0
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int bst_lesser(const binary_tree_t *node, int val)
 {
-	if (tree == NULL)
+	if (node == NULL)
+		return (1);
+	if (node->n >= val)
 		return (0);
-	return (1 + binary_tree_size(tree->left) +
-		binary_tree_size(tree->right));
+
+	return (bst_lesser(node->left, val) &&
+		bst_lesser(node->right, val));
 }
+
 /**
- * tree_preorder - goes through a binary tree using pre-order traversal
- * @array: array to store node values
- * @index: index of array
- * @tree: pointer to the root node of the tree
-*/
-void tree_preorder(int *array, int *index, const binary_tree_t *tree)
+ * bst_greater - BST validator helper function greater
+ * @node: node to compare
+ * @val: value to compare with
+ * Return: 1 if valid BST, else 0
+ */
+int bst_greater(const binary_tree_t *node, int val)
 {
-	if (tree == NULL)
-		return;
+	if (node == NULL)
+		return (1);
+	if (node->n <= val)
+		return (0);
 
-	array[(*index)++] = tree->n;
-	tree_preorder(array, index, tree->left);
-	tree_preorder(array, index, tree->right);
+	return (bst_greater(node->left, val) &&
+		bst_greater(node->right, val));
 }
 /**
- * find_max - finds the max value of a binary tree
- * @tree: pointer to root of tree to be checked
- *
- * Return: max value in a binary tree
-*/
-int find_max(const binary_tree_t *tree)
-{
-	int max_value, left_max, right_max;
-
-	if (tree == NULL)
-		return (-2147483648);
-
-	max_value = tree->n;
-
-
-	left_max = find_max(tree->left);
-	if (left_max > max_value)
-		max_value = left_max;
-
-
-	right_max = find_max(tree->right);
-	if (right_max > max_value)
-		max_value = right_max;
-
-	return (max_value);
-}
-/**
- * find_min - finds the min value of a binary tree
- * @tree: pointer to root of tree to be checked
- *
- * Return: min value in a binary tree
-*/
-int find_min(const binary_tree_t *tree)
-{
-	int min_value, left_min, right_min;
-
-	if (tree == NULL)
-		return (2147483647);
-
-	min_value = tree->n;
-
-
-	left_min = find_min(tree->left);
-	if (left_min < min_value)
-		min_value = left_min;
-
-
-	right_min = find_min(tree->right);
-	if (right_min < min_value)
-		min_value = right_min;
-
-	return (min_value);
-}
-/**
- * binary_tree_is_bst - checks if a binary tree is a BST
+ * binary_tree_is_bst - Check if a binary tree is a valid binary search tree
  * @tree: pointer to root node of tree to check
- *
- * Return: 1 if is a BST, 0 otherwise
-*/
+ * Return: 1 if valid BST, else 0
+ */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	int left_max, right_min, l_sub, r_sub, root, *array = NULL, index = 0;
-	size_t i, j, size;
-
 	if (tree == NULL)
 		return (0);
-	size = binary_tree_size(tree);
-	if (size == 1)
-		return (1);
-	array = malloc(sizeof(int) * size);
-	tree_preorder(array, &index, tree);
-	for (i = 0; i < size; i++)
-	{
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[i] == array[j])
-			{
-				free(array);
-				return (0);
-			}
-		}
-	}
-	free(array);
-	root = tree->n;
-	if (tree->left)
-	{
-		left_max = find_max(tree->left);
-		if (left_max > root)
-			return (0);
-	}
-	if (tree->right)
-	{
-		right_min = find_min(tree->right);
-		if (right_min < root)
-			return (0);
-	}
-	l_sub = tree->left ? binary_tree_is_bst(tree->left) : 1;
-	r_sub = tree->right ? binary_tree_is_bst(tree->right) : 1;
-	return (l_sub && r_sub);
+
+	return (bst_lesser(tree->left, tree->n) &&
+		bst_greater(tree->right, tree->n));
 }
